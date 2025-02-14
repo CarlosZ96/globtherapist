d2fd62a6825df43a04534709c3b9c726e0958393
Carlos <c4r1oszambrano@gmail.com>
2025-02-14 17:48:51 -0500

    Update UserInfo.jsx
    Primeros pasos de la sección de user para ver citas.

--- src/Components/UserInfo.jsx
+++ src/Components/UserInfo.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 const UserInfo = () => {
   return (
     <div>
-      <h1>Username</h1>
+      <div className="user-win-name-cont">
+        <h1>Username</h1>
+      </div>
+      <div>
+        <p>
+          Tipo de terapia:
+          theratype
+          mont date
+          year
+          a las time
+        </p>
+        <p>
+          pro asigando/a:
+          proname.
+        </p>
+      </div>
     </div>
   );
 };
