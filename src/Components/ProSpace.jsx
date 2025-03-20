6d2847764c1da4de9304e2bb59bc5bcee25bd186
Carlos <c4r1oszambrano@gmail.com>
2025-03-20 11:45:58 -0500

    Update ProSpace.jsx

--- src/Components/ProSpace.jsx
+++ src/Components/ProSpace.jsx
@@ -1,17 +1,55 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import { collection } from 'firebase/firestore';
-import { db } from '../firebase';
+import React, { useState, useEffect } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import { db, auth } from '../firebase';
 import Calendar from './Calendar/CalendarWithToggle';
 import ProData from './ProData';
 import Hdv from './Hdv';
 import '../stylesheets/prospace.css';
 
 const ProSpace = () => {
-  const usersCollection = collection(db, 'pros');
+  const [status, setStatus] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        try {
+          const proDocRef = doc(db, 'pros', user.uid);
+          const docSnap = await getDoc(proDocRef);
+          if (docSnap.exists()) {
+            const userData = docSnap.data();
+            setStatus(userData.status);
+          } else {
+            setStatus(null);
+          }
+        } catch (error) {
+          console.error('Error al obtener los datos del pro:', error);
+          setStatus(null);
+        }
+      } else {
+        // Si no hay usuario autenticado
+        setStatus(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="prospace-cont">
-      <Calendar collection="pros" />
+      {status === 'aprobado' ? (
+        <Calendar collection="pros" />
+      ) : (
+        <div>
+          <h1>Estamos validando tus datos</h1>
+        </div>
+      )}
       <ProData />
       <Hdv />
     </div>
