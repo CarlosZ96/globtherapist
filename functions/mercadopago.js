aed2ac2108e512890a3d09163986ac02b44c04d5
Carlos <c4r1oszambrano@gmail.com>
2025-04-19 18:31:44 -0500

    Mp PSE working
    Mp PSE working

--- functions/mercadopago.js
+++ functions/mercadopago.js
@@ -4,7 +4,7 @@ const { MercadoPagoConfig, Payment, PaymentMethod } = require('mercadopago');
 const cors = require('cors')({ origin: true });
 
 const client = new MercadoPagoConfig({
-  accessToken: process.env.MP_ACCESS_TOKEN || 'TEST-2400667744553776-031717-f3674df0979637213ae96babb278b9e9-313341255',
+  accessToken: process.env.MP_ACCESS_TOKEN,
 });
 
 const payment = new Payment(client);
