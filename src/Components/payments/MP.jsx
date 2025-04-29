0fe7954decebfad0ab52f9c7fae40c5f8b250dc0
Carlos <c4r1oszambrano@gmail.com>
2025-04-29 12:30:14 -0500

    Update MP.jsx

--- src/Components/payments/MP.jsx
+++ src/Components/payments/MP.jsx
@@ -21,10 +21,10 @@ const MP = ({
   const navigate = useNavigate();
 
   const therapyPrices = {
-    mental: 80000,
-    fisica: 70000,
-    lenguaje: 55000,
-    ocupacional: 41000,
+    mental: 1800,
+    fisica: 700,
+    lenguaje: 500,
+    ocupacional: 1000,
   };
 
   // Verificar parámetros de URL al montar el componente
